ed40941aabf15c2edf06567d310d4b91b2c24164
Claes Hallström <hallstrom.claes@gmail.com>
2020-05-15 21:53:31 +0200

    Refactor get stock functions
    Refactor get_stock and get_stock_async to use the same structure and to
    handle the same type of exceptions.

--- pyavanza/__init__.py
+++ pyavanza/__init__.py
@@ -1,10 +1,11 @@
 """A Python wrapper around the Avanza mobile API."""
 import json
 import logging
-import urllib.request
+import urllib
 
-from aiohttp import ClientSession
+import aiohttp
 
+LOGGER = logging.getLogger(__name__)
 AVANZA_API_BASE_URL = "https://www.avanza.se/_mobile/market"
 AVANZA_API_STOCK_URL = AVANZA_API_BASE_URL + "/stock/{id}"
 
@@ -12,20 +13,24 @@ AVANZA_API_STOCK_URL = AVANZA_API_BASE_URL + "/stock/{id}"
 def get_stock(id: int) -> dict:
     """Get latest information of a stock."""
     url = AVANZA_API_STOCK_URL.format(id=id)
-    req = urllib.request.Request(url)
     try:
-        resp = urllib.request.urlopen(req).read()
+        resp = urllib.request.urlopen(url).read()
         return json.loads(resp.decode())
-    except Exception as e:
-        logging.getLogger(__name__).error(e)
-        return {}
+    except urllib.error.HTTPError as e:
+        LOGGER.warning("HTTP Error %d: %s" % (e.code, e.reason))
+    except urllib.error.URLError as e:
+        LOGGER.warning("URL Error: %s" % (e.reason))
+    return {}
 
 
-async def get_stock_async(session: ClientSession, id: int) -> dict:
+async def get_stock_async(session: aiohttp.ClientSession, id: int) -> dict:
     """Get latest information of a stock asynchronously."""
     url = AVANZA_API_STOCK_URL.format(id=id)
-    async with session.get(url) as resp:
-        if resp.status == 200:
-            return await resp.json()
-        else:
-            return {}
+    try:
+        resp = await session.get(url, raise_for_status=True)
+        return await resp.json()
+    except aiohttp.ClientResponseError as e:
+        LOGGER.warning("HTTP Error %d: %s" % (e.status, e.message))
+    except aiohttp.ClientConnectionError as e:
+        LOGGER.warning("URL Error [Errno %d] %s" % (e.errno, e.strerror))
+    return {}
