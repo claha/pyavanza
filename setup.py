c1e1fcf76f1bf44e19ba670e97bc47deb556ccb1
Claes Hallström <hallstrom.claes@gmail.com>
2020-05-16 22:03:53 +0200

    Distribute and package type information
    Add the file py.typed and update setup.py to distribute and package type
    information according to PEP 561. For mypy to able to find the installed
    package, zip_safe=False is needed.

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ EMAIL = "hallstrom.claes@gmail.com"
 DESCRIPTION = "A Python wrapper around the Avanza mobile API"
 
 PACKAGES = find_packages()
+PACKAGE_DATA = {"pyavanza": ["py.typed"]}
 
 REQUIRES = [
     "aiohttp>=3.6.1",
@@ -30,6 +31,8 @@ setup(
     license=LICENSE,
     url=URL,
     packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    zip_safe=False,
     install_requires=REQUIRES,
     classifiers=CLASSIFIERS,
 )
