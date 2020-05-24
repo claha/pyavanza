4cd5a2a3b1761c7a6507d3ab90d3ebf79155998a
Claes Hallström <hallstrom.claes@gmail.com>
2020-05-24 23:50:22 +0200

    Return list of instruments when searching
    New class Instrument to hold search result.

--- tests/test_search.py
+++ tests/test_search.py
@@ -54,6 +54,56 @@ class TestSearch(unittest.TestCase):
         )
 
     @patch("pyavanza.urllib.request.urlopen")
+    def test_search_success_no_hits(self, mock_urlopen):
+        """Test a successful request and response without hits."""
+        query = "test"
+        limit = 10
+        instrument = pyavanza.InstrumentType.STOCK
+        mock_urlopen.return_value.read.return_value = json.dumps(
+            {"totalNumberOfHits": 0, "hits": []}
+        ).encode()
+
+        instruments = pyavanza.search(query, limit=limit, instrument=instrument)
+        self.assertEqual(len(instruments), 0)
+        mock_urlopen.assert_called_once_with(
+            pyavanza.AVANZA_API_SEARCH_INSTRUMENT_URL.format(
+                instrument=instrument.value, query=query, limit=limit
+            )
+        )
+
+    @patch("pyavanza.urllib.request.urlopen")
+    def test_search_success_low_limit(self, mock_urlopen):
+        """Test a successful request and response, limit is lower than number of hits."""
+        query = "test"
+        limit = 1
+        mock_urlopen.return_value.read.return_value = json.dumps(
+            {
+                "totalNumberOfHits": 2,
+                "hits": [
+                    {
+                        "instrumentType": "STOCK",
+                        "numberOfHits": 1,
+                        "topHits": [
+                            {
+                                "tradable": True,
+                                "id": "1234",
+                                "tickerSymbol": "TEST",
+                                "name": "Test",
+                            }
+                        ],
+                    },
+                    {"instrumentType": "FUND", "numberOfHits": 1},
+                ],
+            }
+        ).encode()
+
+        instruments = pyavanza.search(query, limit=limit)
+        self.assertEqual(len(instruments), 1)
+        mock_urlopen.assert_called_once_with(
+            pyavanza.AVANZA_API_SEARCH_URL.format(query=query, limit=limit)
+        )
+
+    @patch("pyavanza.urllib.request.urlopen")
     def test_search_success(self, mock_urlopen):
         """Test a successful request and response."""
         query = "test"
@@ -63,18 +113,29 @@ class TestSearch(unittest.TestCase):
             {
                 "totalNumberOfHits": 1,
                 "hits": [
-                    {"instrumentType": "STOCK", "numberOfHits": 1, "topHits": [{}]}
+                    {
+                        "instrumentType": instrument.value,
+                        "numberOfHits": 1,
+                        "topHits": [
+                            {
+                                "tradable": True,
+                                "id": "1234",
+                                "tickerSymbol": "TEST",
+                                "name": "Test",
+                            }
+                        ],
+                    }
                 ],
             }
         ).encode()
 
-        data = pyavanza.search(query, limit=limit, instrument=instrument)
-        self.assertEqual(len(data), 2)
-        self.assertEqual(data["totalNumberOfHits"], 1)
-        self.assertTrue(len(data["hits"]), 1)
-        self.assertTrue(data["hits"][0]["instrumentType"], instrument.value)
-        self.assertTrue(data["hits"][0]["numberOfHits"], 1)
-        self.assertTrue(len(data["hits"][0]["topHits"]), 1)
+        instruments = pyavanza.search(query, limit=limit, instrument=instrument)
+        self.assertEqual(len(instruments), 1)
+        self.assertEqual(instruments[0].type, instrument)
+        self.assertTrue(instruments[0].tradable)
+        self.assertEqual(instruments[0].id, "1234")
+        self.assertEqual(instruments[0].ticker_symbol, "TEST")
+        self.assertEqual(instruments[0].name, "Test")
         mock_urlopen.assert_called_once_with(
             pyavanza.AVANZA_API_SEARCH_INSTRUMENT_URL.format(
                 instrument=instrument.value, query=query, limit=limit
